4094901c0ae854b186c26a54225df61c668ae1ea
Rachit Sharma Workspace..🌱🌱 <144218247+RachitSham@users.noreply.github.com>
2025-06-08 01:10:49 +0530

    Delete BootProj-EmployeeInCompanyUsingRestAPI directory

--- BootProj-EmployeeInCompanyUsingRestAPI/src/main/java/com/nt/utility/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.nt.utility;
-
-public class Constants {
-
-	public static final String SUCCESS = "Success";
-	public static final String FAILED = "Failed";
-	public static final String FAILURE = "Failure";
-}
